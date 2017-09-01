3cc5f94ac9a4bfb4c3f2a2c3cf1d1fe05ddfc688
Alexandru Muresan <amuresan@cloudbasesolutions.com>
2017-09-01 15:01:23 +0300

    Adds devstack oswin-tempest-plugin plugin
    This devstack plugin will allow installing oswin-tempest-plugin.
    
    Change-Id: I2f0daa7cdef99b452c17d5525d08c0c3b4e3fd42

---
+++ devstack/plugin.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIR_OSWIN_TEMPEST_PLUGIN=$DEST/oswin_tempest_plugin
+
+if [[ "$1" == "stack" && "$2" == "install" ]]; then
+  cd $DIR_OSWIN_TEMPEST_PLUGIN
+  echo "Installing oswin-tempest-plugin"
+  setup_develop $DIR_OSWIN_TEMPEST_PLUGIN
+
+  echo "Sucessfully installed oswin-tempest-plugin"
+
+fi
